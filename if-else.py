37ecce69de0c621805e888bfc50b61083e3f6deb
Pallab Nandi <pallab.nandi080304@gmail.com>
2025-03-22 19:43:34 +0530

    if else

---
+++ if-else.py
@@ -0,0 +1,12 @@
+i = 10
+
+ # Checking if i is greater than 15
+if (i > 15):
+    print("10 is less than 15")
+elif (i == 15):
+    print("i is 15")
+elif (i <= 15):
+    print("i is smaller tha 15")
+else:
+    print("var me ja")
+    
